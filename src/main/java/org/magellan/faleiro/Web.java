a7053ca14abb4331f74235f56d4d024773b960f8
Dylan Dumesnil <dadumesnil@gmail.com>
2016-01-12 19:28:54 -0800

    Updated POM to use java 8 compiler and created Web API skeleton

--- src/main/java/org/magellan/faleiro/Web.java
+++ src/main/java/org/magellan/faleiro/Web.java
@@ -1,8 +1,55 @@
 package org.magellan.faleiro;
 
+import spark.Request;
+import spark.Response;
+import spark.Spark;
+
 public class Web {
 
     public static void main(String[] args) {
-	// write your code here
+        initWebRoutes();
+    }
+
+    private static void initWebRoutes() {
+        Spark.post("/api/job", (req, res) -> createJob(req, res));
+        Spark.put("/api/job/*/status", (req, res) -> updateJobStatus(req, res));
+        Spark.patch("/api/job/*", (req, res) -> modifyJob(req, res));
+        Spark.delete("/api/job", (req, res) -> destroyJob(req, res));
+        Spark.get("/api/jobs", (req, res) -> getJobList(req, res));
+        Spark.get("/api/job/*", (req, res) -> getJob(req, res));
+        Spark.get("/api/job/*/tasks", (req, res) -> getJobTaskList(req, res));
+        Spark.get("/api/job/*/task/*", (req, res) -> getJobTask(req, res));
+    }
+
+    private static String createJob(Request req, Response res) {
+        throw new UnsupportedOperationException();
+    }
+
+    private static String updateJobStatus(Request req, Response res) {
+        throw new UnsupportedOperationException();
+    }
+
+    private static String modifyJob(Request req, Response res) {
+        throw new UnsupportedOperationException();
+    }
+
+    private static String destroyJob(Request req, Response res) {
+        throw new UnsupportedOperationException();
+    }
+
+    private static String getJobList(Request req, Response res) {
+        throw new UnsupportedOperationException();
+    }
+
+    private static String getJob(Request req, Response res) {
+        throw new UnsupportedOperationException();
+    }
+
+    private static String getJobTaskList(Request req, Response res) {
+        throw new UnsupportedOperationException();
+    }
+
+    private static String getJobTask(Request req, Response res) {
+        throw new UnsupportedOperationException();
     }
 }
