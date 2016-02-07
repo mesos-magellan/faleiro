9289cd9b5057dd843fda77e0f2c0338e6e142616
Dylan Dumesnil <dadumesnil@gmail.com>
2016-02-07 13:39:47 -0800

    Improved run.sh

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ export LIBPROCESS_IP="10.144.144.1"
 export PRINCIPAL="mesos_master"
 export FRAMEWORK_USER="magellan"
 echo "************************* BUILDING WITH MAVEN ************************* "
-mvn package "$@"
+mvn package $@
 echo "************************* RUNNING FALEIRO ************************* "
 java -cp target/faleiro-1.0-SNAPSHOT.jar org.magellan.faleiro.Web
