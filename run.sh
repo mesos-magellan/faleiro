b1da71d1456af1ce92455721c9ab8c76fe6d1ff8
Dylan Dumesnil <dadumesnil@gmail.com>
2016-02-07 13:39:17 -0800

    Improved run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,9 @@
-mvn package
+#!/usr/bin/env bash
+echo "************************* SETTING ENV VARS ************************* "
+export LIBPROCESS_IP="10.144.144.1"
+export PRINCIPAL="mesos_master"
+export FRAMEWORK_USER="magellan"
+echo "************************* BUILDING WITH MAVEN ************************* "
+mvn package "$@"
+echo "************************* RUNNING FALEIRO ************************* "
 java -cp target/faleiro-1.0-SNAPSHOT.jar org.magellan.faleiro.Web
