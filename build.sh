62f4c3a88194f0574064cb2200e246158fb7f0cc
Dylan Dumesnil <dadumesnil@gmail.com>
2016-03-25 21:47:58 -0700

    Modularized build script, added jacoco for coverage reports

--- build.sh
+++ build.sh
@@ -1,15 +1,2 @@
 #!/usr/bin/env bash
-echo "************************* BUILDING WITH MAVEN ************************* "
-echo "************************* SETTING ENV VARS ************************* "
-export PRINCIPAL="mesos_master"
-export FRAMEWORK_USER="vagrant"
-export MASTER_ADDRESS="10.144.144.10:5050"
-# http://askubuntu.com/a/560466/165699
-export LIBPROCESS_IP="$(ip addr show eth1 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)"
-export LIBPROCESS_PORT="5055"
-export ZK_IP="127.0.0.1"
-export ZK_PORT="2181"
-export ZKNODE_PATH="/faleiro"
-export ATOMIX_LOGS_DIR="logs"
-export EXECUTOR_PATH="/usr/local/bin/enrique"
-mvn package $@
+./env-mvn.sh package
