7767a1ee992565fcf060b99d9e5cd4a315174c93
hfaran <hamzafaran@outlook.com>
2016-03-27 14:38:19 -0700

    Check both ETH1_IP and TUN0_IP

--- run_supervisor.sh
+++ run_supervisor.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
-export LIBPROCESS_IP="$ETH1_IP"
+set -x
+
+if [[ $ETH1_IP ]]; then
+  export LIBPROCESS_IP="$ETH1_IP"
+elif [[ $TUN0_IP ]]; then
+  export LIBPROCESS_IP="$TUN0_IP"
+else
+  echo "Neither ETH1_IP nor TUN0_IP was set!" 1>&2
+  exit 1
+fi
 supervisord
 supervisorctl reload
