11897239d7d5ce5322a1135c9e18e9ec9f3242cb
hfaran <hamzafaran@outlook.com>
2016-03-27 17:47:42 -0700

    Separate supervisord confs for linode and regular

--- run_supervisor.sh
+++ run_supervisor.sh
@@ -3,14 +3,16 @@
 set -x
 
 if [[ `ip addr show eth1` ]]; then
-  export LIBPROCESS_IP="$(ip addr show eth1 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)"
-  export MASTER_ADDRESS="10.144.144.10:5050"
+    export LIBPROCESS_IP="$(ip addr show eth1 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)"
+    export MASTER_ADDRESS="10.144.144.10:5050"
+    supervisord -c supervisord.conf
+    supervisorctl -c supervisord.conf reload
 elif [[ `ip addr show tun0` ]]; then
-  export LIBPROCESS_IP="$(ip addr show tun0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)"
-  export MASTER_ADDRESS="10.8.0.10:5050"
+    export LIBPROCESS_IP="$(ip addr show tun0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)"
+    export MASTER_ADDRESS="10.8.0.10:5050"
+    supervisord -c supervisord_linode.conf
+    supervisorctl -c supervisord_linode.conf reload
 else
-  echo "Neither ETH1_IP nor TUN0_IP was set!" 1>&2
-  exit 1
+    echo "Neither ETH1_IP nor TUN0_IP was set!" 1>&2
+    exit 1
 fi
-supervisord
-supervisorctl reload
